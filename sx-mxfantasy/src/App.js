9492a38f0d3ae68b2f122d3be79edd4feebf2f50
Justin Lee <you@example.com>
2023-02-08 13:12:06 -0800

    Adding files for sx-mxfantasy project

--- sx-mxfantasy/src/App.js
+++ sx-mxfantasy/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, useLocation } from "react-router-dom";
+import LandingPage from './LandingPage';
+import Picks from './Picks';
+import "./App.css";
 
-function App() {
+const App = () => {
+  const location = useLocation();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/picks" component={Picks} />
     </div>
   );
-}
+};
 
 export default App;
