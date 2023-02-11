58afce3dbec916291693c2bba1dfd8ad95a25f5d
Justin Lee <you@example.com>
2023-02-10 20:03:54 -0800

    update

--- sx-mxfantasy/src/pages/Picks.js
+++ sx-mxfantasy/src/pages/Picks.js
@@ -6,32 +6,35 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
 import '../pages/Picks.css';
 
-
 const names = [
-'Devyn', 'Brian', 'Justin', 'Beth', 'Russ', 'Rob', 'Brando', 'Pete', 'Bob', 'Chriatin', 'Jaimie'
+'Devyn', 'Brian', 'Brando', 'Christina', 'Jaimie', 'Justin', 'Bethann', 'Bob', 'Pasky', 'Rob','Pete', 'Russ` ,'
 ];
+const numbers = [601, 601, 594, 594, 592, 586, 586, 584, 582, 581, 577, 551];
 
 export default function AccessibleTable() {
-  return (
-    <TableContainer component={Paper}>
-      <Table aria-label="table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell align="right">Number</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {names.map((name, index) => (
-            <TableRow key={name}>
-              <TableCell>{name}</TableCell>
-              <TableCell align="right">{index + 1}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+return (
+<TableContainer component={Paper}>
+<Table aria-label="table">
+<TableHead>
+<TableRow>
+<TableCell>Name</TableCell>
+<TableCell align="right">Number</TableCell>
+</TableRow>
+</TableHead>
+<TableBody>
+{names.map((name, index) => (
+<TableRow key={name}>
+<TableCell>
+  <Button>{name}</Button>
+</TableCell>
+<TableCell align="right">{numbers[index]}</TableCell>
+</TableRow>
+))}
+</TableBody>
+</Table>
+</TableContainer>
+);
 }
