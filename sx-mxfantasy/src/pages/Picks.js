e67258272f3d4f90b424560b55d540c7cd261c27
Justin Lee <you@example.com>
2023-02-10 22:46:06 -0800

    new update

--- sx-mxfantasy/src/pages/Picks.js
+++ sx-mxfantasy/src/pages/Picks.js
@@ -1,40 +1,36 @@
 import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Button from '@mui/material/Button';
 import '../pages/Picks.css';
 
-const names = [
-'Devyn', 'Brian', 'Brando', 'Christina', 'Jaimie', 'Justin', 'Bethann', 'Bob', 'Pasky', 'Rob','Pete', 'Russ` ,'
-];
+const names = ['Devyn', 'Brian', 'Brando', 'Christina', 'Jaimie', 'Justin', 'Bethann', 'Bob', 'Pasky', 'Rob', 'Pete', 'Russ'];
 const numbers = [601, 601, 594, 594, 592, 586, 586, 584, 582, 581, 577, 551];
 
 export default function AccessibleTable() {
-return (
-<TableContainer component={Paper}>
-<Table aria-label="table">
-<TableHead>
-<TableRow>
-<TableCell>Name</TableCell>
-<TableCell align="right">Number</TableCell>
-</TableRow>
-</TableHead>
-<TableBody>
-{names.map((name, index) => (
-<TableRow key={name}>
-<TableCell>
-  <Button>{name}</Button>
-</TableCell>
-<TableCell align="right">{numbers[index]}</TableCell>
-</TableRow>
-))}
-</TableBody>
-</Table>
-</TableContainer>
-);
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleChange = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Accordion expanded={expanded} onChange={handleChange}>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+      >
+        Show Names
+      </AccordionSummary>
+      <AccordionDetails>
+        {names.map((name, index) => (
+          <React.Fragment key={name}>
+            <Button>{name}</Button>
+            {numbers[index]}
+          </React.Fragment>
+        ))}
+      </AccordionDetails>
+    </Accordion>
+  );
 }
