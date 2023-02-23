3f3d8f8aadefda01379fc37f869274db7f8ef6f5
Justin Lee <you@example.com>
2023-02-22 20:31:49 -0800

    new code

--- sx-mxfantasy/src/components/Header.js
+++ sx-mxfantasy/src/components/Header.js
@@ -1,13 +1,36 @@
 import React from 'react';
-import { Container, Paper } from '@mui/material';
+import { Container, AppBar, Toolbar, Button, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <Container style={{ width: '100%', position: 'fixed', top: '0', zIndex: '1' }}>
-      <Paper elevation={3} style={{ backgroundColor: "transparent" }}>
-      </Paper>
+    <Container style={{ width: '100%', position: 'flex', top: '0', zIndex: '1' }}>
+      <AppBar position="static" style={{ backgroundColor: '#ffff', boxShadow: 'none' }}>
+        <Toolbar style={{ justifyContent: 'space-between' }}>
+          <Typography variant="h5" component={Link} to="/" color="secondary" style={{ fontFamily: 'Lobster Two', textDecoration: 'none' }}>
+            Atmosphere
+          </Typography>
+          <div>
+            <Button component={Link} to="/picks" color="secondary" style={{ fontFamily: 'Montserrat', fontWeight: 'bold', marginRight: 16 }}>
+              My Picks
+            </Button>
+            <Button component={Link} to="/standings" color="secondary" style={{ fontFamily: 'Montserrat', fontWeight: 'bold', marginRight: 16 }}>
+              Standings
+            </Button>
+            <Button component={Link} to="/rules" color="secondary" style={{ fontFamily: 'Montserrat', fontWeight: 'bold', marginRight: 16 }}>
+              Rules
+            </Button>
+            <Button component={Link} to="/login" variant="outlined" color="secondary" style={{ fontFamily: 'Montserrat', fontWeight: 'bold' }}>
+              Login
+            </Button>
+          </div>
+        </Toolbar>
+      </AppBar>
     </Container>
   );
 };
 
 export default Header;
+
+
+
--- sx-mxfantasy/src/pages/LandingPage.js
+++ sx-mxfantasy/src/pages/LandingPage.js
@@ -1,40 +1,87 @@
 import React from 'react';
-import { Typography, Box, Card, Button } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Card, CardMedia, CardContent, Typography, Grid, Button, TextField, Divider } from '@mui/material';
 import Header from '../components/Header';
-import backgroundImage from '../assets/sx2.png';
-import '../pages/LandingPage.css';
+import './LandingPage.css';
 
+const sports = [
+  { id: 1, name: 'Formula 1', imageUrl: 'https://source.unsplash.com/featured/?formula1' },
+  { id: 2, name: 'NASCAR', imageUrl: 'https://source.unsplash.com/featured/?nascar' },
+  { id: 3, name: 'MotoGP', imageUrl: 'https://source.unsplash.com/featured/?motogp' },
+  { id: 4, name: 'Supercross', imageUrl: 'https://source.unsplash.com/featured/?motogp' },
+  { id: 5, name: 'Motocross', imageUrl: 'https://source.unsplash.com/featured/?motogp' },
+  { id: 6, name: 'Surfing', imageUrl: 'https://source.unsplash.com/featured/?motogp' },
+  { id: 7, name: 'Nascar', imageUrl: 'https://source.unsplash.com/featured/?motogp' },
+  { id: 8, name: 'Indycar', imageUrl: 'https://source.unsplash.com/featured/?motogp' },
+];
 
 const LandingPage = () => {
   return (
-    <Box component="section" display="flex" flexDirection="column" alignItems="center" justifyContent="center" style={{
-      backgroundImage: `url(${backgroundImage})`,
-      backgroundSize: 'cover',
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: 'center',
-      height: '100vh'
-    }}>
+    <div className="landing-page">
       <Header />
-      <main>
-      <Card className="metallic-card">
-  <Typography variant="h6" align="center">Welcome to Fantasy SX</Typography>
-  <Typography variant="body1" align="center">
-    Start picking your team and compete with your friends
-  </Typography>
-  <Box mt={2} display="flex" justifyContent="center">
-    <Button variant="contained" color="primary" className="pulsing-button">
-      <Link to="/Picks">Sign Up</Link>
-    </Button>
-  </Box>
-</Card>
-
-
-      </main>
-      <footer>
-        <Typography variant="body1" align="center">Copyright © Fantasy SX 2023</Typography>
-      </footer>
-    </Box>
+      <Grid container justifyContent="center">
+        {sports.map((sport) => (
+          <Grid item key={sport.id}>
+            <Card className="card">
+              <CardMedia className="media" image={sport.imageUrl} title={sport.name} />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  {sport.name}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+      <div className="features-section">
+        <Grid container justifyContent="center">
+          <Grid item xs={12} md={6}>
+            <Typography variant="h5" align="center" gutterBottom>
+              Features
+            </Typography>
+            <Typography variant="body1" align="center">
+              Here are some of the key features of our app:
+            </Typography>
+            <ul>
+              <li>Customizable leagues with friends</li>
+              <li>Live scoring and leaderboard updates</li>
+              <li>Detailed stats and analytics</li>
+              <li>Easy team management and roster changes</li>
+            </ul>
+            <Grid container justifyContent="center">
+              <Grid item>
+                <Button variant="contained" color="primary" href="/signup">
+                  Sign Up Now
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </div>
+      <div className="message-board-section">
+        <Typography variant="h5" align="center" gutterBottom>
+          Join the Discussion
+        </Typography>
+        <Typography variant="body1" align="center">
+          Have a question or want to share your thoughts? Leave a message below:
+        </Typography>
+        <Grid container justifyContent="center">
+          <Grid item xs={12} md={6}>
+            <form className="form">
+              <TextField className="input" label="Name" variant="outlined" />
+              <TextField className="input" label="Message" variant="outlined" multiline rows={4} />
+              <Button variant="contained" color="primary">
+                Submit
+              </Button>
+            </form>
+            <Divider />
+            {/* Message board */}
+          </Grid>
+        </Grid>
+      </div>
+    </div>
   );
 };
 
