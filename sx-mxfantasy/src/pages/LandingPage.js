d3cd1d45b2ca1a8a280d3899d0d71b8a73ab6c57
Justin Lee <you@example.com>
2023-02-23 20:58:27 -0800

    newest updates

--- sx-mxfantasy/src/pages/LandingPage.js
+++ sx-mxfantasy/src/pages/LandingPage.js
@@ -1,88 +1,116 @@
 import React from 'react';
-import { Card, CardMedia, CardContent, Typography, Grid, Button, TextField, Divider } from '@mui/material';
-import Header from '../components/Header';
+import { TextField } from '@mui/material';
+import { Grid, Typography, Button, Divider } from '@mui/material';
+import Header from '../components/Header/Header';
+import { OutlinedCard } from '../components/card';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import './LandingPage.css';
 
 const sports = [
-  { id: 1, name: 'Formula 1', imageUrl: 'https://source.unsplash.com/featured/?formula1' },
-  { id: 2, name: 'NASCAR', imageUrl: 'https://source.unsplash.com/featured/?nascar' },
-  { id: 3, name: 'MotoGP', imageUrl: 'https://source.unsplash.com/featured/?motogp' },
-  { id: 4, name: 'Supercross', imageUrl: 'https://source.unsplash.com/featured/?motogp' },
-  { id: 5, name: 'Motocross', imageUrl: 'https://source.unsplash.com/featured/?motogp' },
-  { id: 6, name: 'Surfing', imageUrl: 'https://source.unsplash.com/featured/?motogp' },
-  { id: 7, name: 'Nascar', imageUrl: 'https://source.unsplash.com/featured/?motogp' },
-  { id: 8, name: 'Indycar', imageUrl: 'https://source.unsplash.com/featured/?motogp' },
+{ id: 1, name: 'Formula 1', imageUrl: 'https://source.unsplash.com/featured/?formula1' },
+{ id: 2, name: 'NASCAR', imageUrl: 'https://source.unsplash.com/featured/?nascar' },
+{ id: 3, name: 'MotoGP', imageUrl: 'https://source.unsplash.com/featured/?motogp' },
+{ id: 4, name: 'Supercross', imageUrl: 'https://source.unsplash.com/featured/?supercross' },
+{ id: 5, name: 'Motocross', imageUrl: 'https://source.unsplash.com/featured/?motocross' },
+{ id: 6, name: 'Surfing', imageUrl: 'https://source.unsplash.com/featured/?surfing' },
+{ id: 7, name: 'Nascar', imageUrl: 'https://source.unsplash.com/featured/?nascar' },
+{ id: 8, name: 'Indycar', imageUrl: 'https://source.unsplash.com/featured/?indycar' },
 ];
 
 const LandingPage = () => {
-  return (
-    <div className="landing-page">
-      <Header />
-      <Grid container justifyContent="center">
-        {sports.map((sport) => (
-          <Grid item key={sport.id}>
-            <Card className="card">
-              <CardMedia className="media" image={sport.imageUrl} title={sport.name} />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  {sport.name}
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
-      <div className="features-section">
-        <Grid container justifyContent="center">
-          <Grid item xs={12} md={6}>
-            <Typography variant="h5" align="center" gutterBottom>
-              Features
-            </Typography>
-            <Typography variant="body1" align="center">
-              Here are some of the key features of our app:
-            </Typography>
-            <ul>
-              <li>Customizable leagues with friends</li>
-              <li>Live scoring and leaderboard updates</li>
-              <li>Detailed stats and analytics</li>
-              <li>Easy team management and roster changes</li>
-            </ul>
-            <Grid container justifyContent="center">
-              <Grid item>
-                <Button variant="contained" color="primary" href="/signup">
-                  Sign Up Now
-                </Button>
-              </Grid>
+return (
+<div className="landing-page">
+
+  {/* Header */}
+  <Header />
+
+  {/* Animated background */}
+  <div className="animated-background">
+    <div className="a"></div>
+    <div id="circle"></div>
+  </div>
+
+  {/* Sports section */}
+  <div className="sports-section">
+    <Typography variant="h5" align="center" gutterBottom>
+      Sports
+    </Typography>
+    <Divider />
+    <Slider infinite={true} slidesToShow={4} slidesToScroll={1} arrows={true} dots={false}>
+      {sports.map((sport) => (
+        <OutlinedCard key={sport.id} imageUrl={sport.imageUrl} name={sport.name} />
+      ))}
+    </Slider>
+  </div>
+
+  {/* Features section */}
+  <div className="features-section">
+    <Grid container justifyContent="center">
+      <Grid item xs={12} md={6}>
+        
+          <Grid container direction="column" alignItems="center">
+            <Grid item>
+              <Typography variant="h5" align="center" gutterBottom>
+                Features
+              </Typography>
+              <Divider />
+            </Grid>
+            <Grid item>
+              <Typography variant="body1" align="center">
+Season-long competition: The competition will run throughout the season of each sports series. Users will accumulate points based on the performance of the athletes they select throughout the season, and the user with the most points at the end of the season will be declared the winner.
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Button variant="contained" color="primary">
+                Learn More
+              </Button>
             </Grid>
           </Grid>
-        </Grid>
-      </div>
-      <div className="message-board-section">
-        <Typography variant="h5" align="center" gutterBottom>
-          Join the Discussion
-        </Typography>
-        <Typography variant="body1" align="center">
-          Have a question or want to share your thoughts? Leave a message below:
-        </Typography>
-        <Grid container justifyContent="center">
-          <Grid item xs={12} md={6}>
-            <form className="form">
-              <TextField className="input" label="Name" variant="outlined" />
-              <TextField className="input" label="Message" variant="outlined" multiline rows={4} />
-              <Button variant="contained" color="primary">
-                Submit
-              </Button>
-            </form>
-            <Divider />
-            {/* Message board */}
-          </Grid>
-        </Grid>
-      </div>
-    </div>
-  );
-};
+    </Grid>
+        
+    </Grid>
+  </div>
 
+  {/* Newsletter section */}
+  <div className="newsletter-section">
+    <Grid container justifyContent="center">
+      <Grid item xs={12} md={6}>
+        
+          <Grid container direction="column" alignItems="center">
+            <Grid item>
+              <Typography variant="h5" align="center" gutterBottom>
+                Subscribe to our Newsletter
+                </Typography>
+          <Divider />
+        </Grid>
+        <Grid item>
+          <Typography variant="body1" align="center">
+            Get the latest news and updates delivered straight to your inbox.
+          </Typography>
+        </Grid>
+        <Grid item>
+          <form noValidate autoComplete="off">
+            <TextField id="outlined-basic" label="Email" variant="outlined" />
+            <Button variant="contained" color="primary">
+              Subscribe
+            </Button>
+          </form>
+        </Grid>
+      </Grid>
+    
+  </Grid>
+</Grid>
+</div>
+{/* Footer */}
+
+  <div className="footer">
+    <Typography variant="body1" align="center">
+      &copy; 2023 Sportsify. All rights reserved.
+    </Typography>
+  </div>
+</div>
+);
+};
 export default LandingPage;
